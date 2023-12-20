142c7bf5529b25f14312f848f669ef0b8288537f
Treesa Antony <tresatritto28@gmail.com>
2023-12-20 21:50:02 +0100

    inital commit

---
+++ src/main/java/com/app/telemedicine/TelemedicineApplication.java
@@ -0,0 +1,13 @@
+package com.app.telemedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedicineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelemedicineApplication.class, args);
+    }
+
+}
